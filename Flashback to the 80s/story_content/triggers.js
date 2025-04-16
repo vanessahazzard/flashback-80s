9b1509330e1f9a1277aa5f3cab5ca7ace2f9372c
vanessahazzard <vanessaht@me.com>
2025-04-16 02:03:24 -0400

    Add files via upload

---
+++ Flashback to the 80s/story_content/triggers.js
@@ -0,0 +1,45 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "67KRhrv9Qlj":
+        Script1();
+        break;
+  }
+}
+
+window.InitExecuteScripts = function()
+{
+var player = GetPlayer();
+var object = player.object;
+var once = player.once;
+var addToTimeline = player.addToTimeline;
+var setVar = player.SetVar;
+var getVar = player.GetVar;
+var update = player.update;
+var pointerX = player.pointerX;
+var pointerY = player.pointerY;
+var showPointer = player.showPointer;
+var hidePointer = player.hidePointer;
+var slideWidth = player.slideWidth;
+var slideHeight = player.slideHeight;
+window.Script1 = function()
+{
+  const target = object('6Fo3yK1CNpf');
+const duration = 750;
+const easing = 'ease-out';
+const id = '6Giyevkp0E0';
+const pulseAmount = 0.07;
+player.addForTriggers(
+id,
+target.animate([
+{ scale: '1' }, { scale: `${1 + pulseAmount}` },
+{ scale: '1' }, { scale: `${1 + pulseAmount}` },
+{ scale: '1' }
+],
+  { fill: 'forwards', duration, easing }
+)
+);
+}
+
+};
